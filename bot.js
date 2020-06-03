7cba646e8a1e03331d8afc99ec3baa7f80973d36
BuildTools <unconfigured@null.spigotmc.org>
2020-06-03 16:30:13 +0200

    move the move command to a seperate function

--- bot.js
+++ bot.js
@@ -26,39 +26,7 @@ bot.on('message', message => {
 		message.channel.send(text.other.test)
 	} else if (message.content.startsWith(">mv") || message.content.startsWith(">move")) {
 		// Move command:
-		// Get channels for moving
-		const cmd = [message.content.split(" ")[0], message.content.substring(message.content.split(" ")[0].length)]
-		const channels = cmd[1].split(">")
-		if (channels.length == 1) {
-			const fromChannel = message.member.voice.channel // Caller's channel
-			const toChannel = message.guild.channels.cache.find(channel => channel.name === channels[0].trim())
-		} else if (channels.length == 2) {
-			//sorry
-			const fromChannel = message.guild.channels.cache.find(channel => channel.name === channels[0].trim())
-			const toChannel = message.guild.channels.cache.find(channel => channel.name === channels[1].trim())
-		} else {
-			const fromChannel = undefined
-			const toChannel = undefined
-			message.channel.send(text.usage.move)
-			return
-		}
-
-		// Verify channels
-		if (fromChannel == undefined || toChannel == undefined) {
-			message.channel.send(text.usage.move)
-			return
-		}
-
-		// Get users in fromChannel
-		const usersToMove = fromChannel.members
-
-		// Verify if users can be moved
-		// Verify caller can move users
-		// Verify users will fit in toChannel
-
-		// Move users to toChannel
-
-		// Reply to original message
+		moveCmd(message)
 	} else if (message.content.startsWith(">rm") || message.content.startsWith(">remove") || message.content.startsWith(">del") || message.content.startsWith(">delete")) {
 		// Delete command:
 		// Get amount of messages to be deleted
@@ -69,5 +37,41 @@ bot.on('message', message => {
 		// Delete messages
 	}
 });
+
+function moveCmd(message) {
+	// Get channels for moving
+	const cmd = [message.content.split(" ")[0], message.content.substring(message.content.split(" ")[0].length)]
+	const channels = cmd[1].split(">")
+	if (channels.length == 1) {
+		const fromChannel = message.member.voice.channel // Caller's channel
+		const toChannel = message.guild.channels.cache.find(channel => channel.name === channels[0].trim())
+	} else if (channels.length == 2) {
+		//sorry
+		const fromChannel = message.guild.channels.cache.find(channel => channel.name === channels[0].trim())
+		const toChannel = message.guild.channels.cache.find(channel => channel.name === channels[1].trim())
+	} else {
+		const fromChannel = undefined
+		const toChannel = undefined
+		message.channel.send(text.usage.move)
+		return
+	}
+
+	// Verify channels
+	if (fromChannel == undefined || toChannel == undefined) {
+		message.channel.send(text.usage.move)
+		return
+	}
+
+	// Get users in fromChannel
+	const usersToMove = fromChannel.members
+
+	// Verify if users can be moved
+	// Verify caller can move users
+	// Verify users will fit in toChannel
+
+	// Move users to toChannel
+
+	// Reply to original message
+}
 
 bot.login(config.token)
