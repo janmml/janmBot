4ae2ad7f71d04fc107c5816eaf60b609f1b1b772
BuildTools <66574284+janmml@users.noreply.github.com>
2020-08-26 12:23:55 +0200

    add >roll command

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@
 const config = require("./config.json")
 const text = require("./text.json")
 const Discord = require("discord.js")
+const crypto = require("crypto")
 
 // Initialize the bot
 const bot = new Discord.Client()
@@ -78,7 +79,19 @@ bot.on("message", message => {
 			message.channel.send(text.fail.delete)
 
 		}
+
+	} else if (message.content.startsWith(">roll")) {
+		// Roll command:
+		try {
+			rollCmd(message)
+
+		} catch (error) {
+			message.channel.send(text.fail.roll)
+
+		}
+
 	}
+
 })
 
 bot.on("messageDelete", message => {
@@ -360,8 +373,393 @@ function helpCmd(message) {
 
 }
 
+function rollCmd(message) {
+	/*
+	* Rolls dice specified using Dice notation
+	* (see https://en.wikipedia.org/wiki/Dice_notation and
+	* https://roll20.zendesk.com/hc/en-us/articles/360037773133)
+	* from the message, then replies with the result.
+	*
+	* The dice to be rolled (in Dice notation) is
+	* the first part of message text after a " ".
+	* 
+	* message - type Message, must be from a guild
+	* 
+	* returns - false on failure, true on success
+	*/
+
+	// Get the dice to be rolled
+	let cmd = message.cleanContent.substring(
+		message.cleanContent.split(" ")[0].length
+	).trim()
+
+	// If the user is asking about supported rolls, send that message
+	if (cmd === "supported") {
+		message.channel.send(text.other.supportedRolls)
+		return
+	}
+
+	let initialCmd = cmd
+
+	// Remove all spaces from cmd
+	cmd = cmd.split(" ").join("")
+
+	// Add default parentheses (needed later on)
+	cmd = "(" + cmd + ")"
+
+
+	// Check the cmd syntax
+	if (cmd.split("(").length != cmd.split(")").length) {
+		// Check for balanced parentheses
+		message.channel.send(text.fail.roll)
+		return
+	} else if (!/[\d%‰‱Fb]/gi.test(cmd)) {
+		// Check if it contains at least one digit (or other die symbol)
+		message.channel.send(text.fail.roll)
+		return
+	}
+
+	// Prepare the variable, which hold the results of the parsing
+	let equations = cmd + "\n"
+
+	// Get the result
+	try {
+		// While (true) loop equivalent, but with a limit of 1000, in case
+		// the break doesn't get reached for some reason
+		for (let i = 0; i < 1000; i++) {
+			// Go through the parentheses, most to least nested
+			let parentheses = {}
+			let levelCounter = 0
+			let maxLevel = 0
+	
+			// Get the indices of all opening parentheses
+			// and their respective levels of nestedness
+			for (let i = 0; i < cmd.length; i++) {
+				if (cmd[i] === "(") {
+					levelCounter++
+					parentheses[i] = levelCounter
+					
+					if (levelCounter > maxLevel) {
+						maxLevel = levelCounter
+	
+					}
+	
+				} else if (cmd[i] === ")") {
+					levelCounter--
+	
+				}
+	
+			}
+
+			// Find the most nested sets of parentheses ...
+			let parenthesisIndices = Object.keys(parentheses).filter(
+				key => parentheses[key] == maxLevel
+			)
+
+			// ... and go through the left-most set
+			let j = parseInt(parenthesisIndices[0]) + 1
+			let k = cmd.indexOf(")", j)
+			let currentSetOfParentheses = cmd.substring(j, k)
+
+			// Process the contents of the parentheses
+			let res = parseDiceNotation(currentSetOfParentheses)
+			equations += cmd.substring(0, j) + res + cmd.substring(k) + "\n"
+			if (res.includes("(")) {
+				cmd = cmd.substring(0, j) + res + cmd.substring(k)
+				cmd = cmd.split(" ").join("")
+				continue
+			}
+
+			// Do the math
+			res = res.split(" ")
+
+			// While (true) loop equivalent, but with a limit of 1000, in case
+			// the break doesn't get reached for some reason
+			let regex = /^[\/÷\*×]$/gi
+			for (let l = 0; l < 1000; l++) {
+				// Do all the division/multiplication
+				if (res.find(m => regex.test(m)) === "*") {
+					// If the left-most division/multiplication is *
+					let n = res.indexOf("*")
+					res.splice(n - 1,
+						3,
+						parseInt(res[n - 1]) * parseInt(res[n + 1])
+					)
+
+				} else if (res.find(m => regex.test(m)) === "×") {
+					// If the left-most division/multiplication is ×
+					let n = res.indexOf("×")
+					res.splice(n - 1,
+						3,
+						parseInt(res[n - 1]) * parseInt(res[n + 1])
+					)
+
+				} else if (res.find(m => regex.test(m)) === "/") {
+					// If the left-most division/multiplication is /
+					let n = res.indexOf("/")
+					res.splice(n - 1,
+						3,
+						parseInt(res[n - 1]) / parseInt(res[n + 1])
+					)
+
+				} else if (res.find(m => regex.test(m)) === "÷") {
+					// If the left-most division/multiplication is ÷
+					let n = res.indexOf("÷")
+					res.splice(n - 1,
+						3,
+						parseInt(res[n - 1]) / parseInt(res[n + 1])
+					)
+
+				} else if (res.find(m => regex.test(m)) === undefined) {
+					// If there aren't any more division/multiplication symbols
+					break
+				}
+
+				// If this looped too many times, assume an error and stop
+				if (l == 999) {
+					message.channel.send(text.fail.roll)
+					return
+				}
+
+			}
+
+			// While (true) loop equivalent, but with a limit of 1000, in case
+			// the break doesn't get reached for some reason
+			regex = /^[+\-–—−]$/gi
+			for (let l = 0; l < 1000; l++) {
+				// Do all the addition/subtraction
+				if (res.find(m => regex.test(m)) === "+") {
+					// If the left-most addition/subtraction is +
+					let n = res.indexOf("+")
+					res.splice(n - 1,
+						3,
+						parseInt(res[n - 1]) + parseInt(res[n + 1])
+					)
+
+				} else if (res.find(m => regex.test(m)) === "-") {
+					// If the left-most addition/subtraction is a hyphen-minus
+					let n = res.indexOf("-")
+					res.splice(n - 1,
+						3,
+						parseInt(res[n - 1]) - parseInt(res[n + 1])
+					)
+
+				} else if (res.find(m => regex.test(m)) === "–") {
+					// If the left-most addition/subtraction is an endash
+					let n = res.indexOf("–")
+					res.splice(n - 1,
+						3,
+						parseInt(res[n - 1]) - parseInt(res[n + 1])
+					)
+
+				} else if (res.find(m => regex.test(m)) === "—") {
+					// If the left-most addition/subtraction is an emdash
+					let n = res.indexOf("—")
+					res.splice(n - 1,
+						3,
+						parseInt(res[n - 1]) - parseInt(res[n + 1])
+					)
+
+				} else if (res.find(m => regex.test(m)) === "−") {
+					// If the left-most addition/subtraction is a minus
+					let n = res.indexOf("−")
+					res.splice(n - 1,
+						3,
+						parseInt(res[n - 1]) - parseInt(res[n + 1])
+					)
+
+				} else if (res.find(m => regex.test(m)) === undefined) {
+					// If there aren't any more addition/subtraction symbols
+					break
+				}
+
+				// If this looped too many times, assume an error and stop
+				if (l == 999) {
+					message.channel.send(text.fail.roll)
+					return
+				}
+
+			}
+
+			// Remove the parentheses
+			res = res.join(" ")
+			cmd = cmd.substring(0, j - 1) + res + cmd.substring(k + 1)
+			equations += cmd + "\n"
+
+			// End the loop if the roll has been completed
+			if (cmd.split("(").length === 1) {
+				break
+			}
+
+			// If this looped too many times, assume an error and stop
+			if (i == 999) {
+				message.channel.send(text.fail.roll)
+				return
+			}
+
+		}
+
+		// Escape asterisks, needed for markdown reasons
+		equations = equations.split("*").join("\\*")
+		initialCmd = initialCmd.split("*").join("\\*")
+
+		// Limit the size of the text variables, if they're too long
+		if (equations.length > 1020) {
+			equations = equations.substring(
+				0,
+				500
+			) + "\n...\n" + equations.substring(
+				equations.length - 500
+			)
+
+		}
+		if (initialCmd.length > 255) {
+			initialCmd = initialCmd.substring(0, 252) + "..."
+
+		}
+
+		// Send the result in an embed
+		let resultEmbed = new Discord.MessageEmbed()
+		.setTitle(cmd)
+		.addField(initialCmd, equations)
+		message.channel.send(resultEmbed).catch(console.log)
+
+	} catch (error) {
+		message.channel.send(text.fail.roll)
+
+	}
+
+}
+
 
 // Utility functions
+function rollDice(amount, sides) {
+	/*
+	* Rolls [amount] of [sides]-sided dice, then adds the results.
+	* Both amount and sides have a limit of 65536,
+	* and both must be positive integers.
+	*
+	* amount - number (integer) - The amount of dice to be rolled.
+	*
+	* sides - number (integer) - The amount of sides each die has.
+	*
+	* Returns an array, the first element being the sum of the dice rolls,
+	* the second element being an array of all the individual rolls,
+	* in the order they were rolled.
+	*/
+
+	// Check if amount and sides aren't too big
+	if (amount > 65536 || sides > 65536) {
+		throw new RangeError("Amount and sides must be less than 65536.")
+	}
+
+	// Check if amount and sides are positive
+	if (amount < 1 || sides < 2) {
+		throw new RangeError("Amount and/or sides are too small.")
+	}
+
+	// Check if amount and sides are ints
+	if (!(Number.isInteger(amount) && Number.isInteger(sides))) {
+		throw new TypeError("Amount and sides must be integers.")
+	}
+
+	// Declare array, which holds rolling results
+	let rolls = [0, []]
+
+	// Roll the dice
+	for (; amount > 0; amount--) {
+		// The following should solve the bias problems mentioned in
+		// https://gist.github.com/joepie91/7105003c3b26e65efcea63f3db82dfba,
+		// in a similar way to crypto-secure-random-digit
+		// (https://www.npmjs.com/package/crypto-secure-random-digit), but
+		// with a flexible maximum value.
+
+		let roll = crypto.randomBytes(2).readUInt16BE(0)
+		const maxRandom = (65536 - (65536 % sides)) - 1
+
+		while (roll > maxRandom) {
+			// Re-roll the random number
+			roll = crypto.randomBytes(2).readUInt16BE(0)
+		}
+
+		roll = (roll % sides) + 1
+	
+		rolls[0] += roll
+		rolls[1].push(roll)
+
+	}
+
+	return rolls
+}
+
+function parseDiceNotation(dn) {
+	/*
+	* Parse a bit of dice notation (without parentheses), and return the
+	* result, rolling dice if necessary.
+	*
+	* dn - string - The bit of dice notation
+	* (see https://en.wikipedia.org/wiki/Dice_notation and
+	* https://roll20.zendesk.com/hc/en-us/articles/360037773133)
+	*
+	* Currently supports the following*:
+	* *Capital letters represent numbers, brackets surround optional parts.
+	* - A
+	* - +, -, *, ×, /, ÷, (, )
+	* - [A]dB
+	* - [A]d%, [A]d‰, [A]d‱
+	*
+	* returns - The result of the parsing of the supplied dice notation,
+	* as a string, after rolling all dice. The returned string will be
+	* a mathematical equation, with addition, subtraction, multiplication and
+	* division. All dice rolls will be converted into such equations as well
+	* (for example "3d8" would be turned into "5 + 4 + 1", then inserted into
+	* the original input string in the place of "3d8").
+	*/
+
+	// Prepare the result-holding variable
+	let res = " "
+
+	// Account for percentile, permille-ile, and permyriad-ile dice
+	dn = dn.split("d%").join("d100")
+		.split("d‰").join("d1000")
+		.split("d‱").join("d10000")
+
+	// Split the dn at all mathematical symbols (keeping those symbols)
+	dn = dn.split(/((?<![+\-–—−*×\/÷])[+\-–—−*×\/÷](?![LH+*×\/÷]))/gi)
+
+	// Go through all logical parts, and if they represent die rolls,
+	// roll those dice, turning them into numbers
+	for (let i of dn) {
+		if (/^[\-–—−]?\d+$/gi.test(i)) {
+			// If it's a number
+			res += i + " "
+
+		} else if (/^[+\-–—−\*×\/÷]$/gi.test(i)) {
+			// If it's a mathematical symbol
+			res += i + " "
+			
+		} else if (/^d\d+$/gi.test(i)) {
+			// If it's a die roll (without the number of dice specified)
+			let dieRoll = rollDice(1,
+				parseInt(i.substring(1))
+			)[1]
+			res += dieRoll.join(" + ") + " "
+			
+		} else if (/^\d+d\d+$/gi.test(i)) {
+			// If it's a die roll (with the number of dice specified)
+			let dieRoll = rollDice(parseInt(i.substring(0, i.indexOf("d"))),
+				parseInt(i.substring(i.indexOf("d") + 1)))[1]
+			res += "(" + dieRoll.join(" + ") + ") "
+			
+		} else {
+			throw new SyntaxError("Logical part not supported: " + i)
+		}
+
+	}
+
+	return res.trim()
+
+}
 
 function getGuildChannels(guild, type, name) {
 	/*
